2ae93ca880f0391fbff4c4b985db566689b6c39e
snerks <martin.github@outlook.com>
2017-02-10 12:16:53 +0000

    NgRedux integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { NgRedux } from 'ng2-redux';
+
+import { IAppState } from './store';
+import { INCREMENT } from './actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'app works!';
+
+  counter = 0;
+  subscription;
+
+  constructor(private ngRedux: NgRedux<IAppState>) {
+    this.subscription = (
+      ngRedux
+        .select<number>('counter')
+        .subscribe(newCounter => this.counter = newCounter)
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  increment() {
+    this.ngRedux.dispatch({ type: INCREMENT });
+  }
 }
